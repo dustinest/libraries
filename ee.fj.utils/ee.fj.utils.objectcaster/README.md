accdaeb310886935bd53d6413271853e5262b427
margus <margus.rebane@gmail.com>
2017-05-30 20:41:52 +0300

    removed objectcaster

--- ee.fj.utils/ee.fj.utils.objectcaster/README.md
+++
@@ -1,1 +0,0 @@
-Under construction and might be removed in the future. Just an idea of basic objects is / as casting
--- ee.fj.utils/ee.fj.utils.objectcaster/src/main/java/ee/fj/utils/objectcaster/TypeCaster.java
+++
@@ -1,6 +0,0 @@
-package ee.fj.utils.objectcaster;
-
-public interface TypeCaster<T> {
-	public boolean is(Object value);
-	public T as(Object value);
-}
