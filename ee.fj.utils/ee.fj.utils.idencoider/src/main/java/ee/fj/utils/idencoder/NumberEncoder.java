a0185ab8dce60459358dc1b35fafc5dd6919d396
margus <margus.rebane@gmail.com>
2019-11-26 04:26:17 +0200

    code cleanup

--- ee.fj.utils/ee.fj.utils.idencoider/src/main/java/ee/fj/utils/idencoder/NumberEncoder.java
+++ ee.fj.utils/ee.fj.utils.idencoider/src/main/java/ee/fj/utils/idencoder/NumberEncoder.java
@@ -1,9 +1,7 @@
 package ee.fj.utils.idencoder;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class NumberEncoder {
 	private final NumericCharactersMap[] charMatrix;
